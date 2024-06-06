e6c4dca7b3046fb2349abbe8dcdc1bb16fd3246d
RITESH RAJ AS <riteshrajas.arulselvan@gmail.com>
2024-06-05 23:38:13 -0400

    Added Vision Documentation Part 1-2

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,6 +2,33 @@ export default {
     logo: <span>Programmer's Handbook</span>,
     project: {
         link: 'https://github.com/Rochester-CS-Club'
-    }
-    // ... other theme options
+    },
+    footer: {
+        text: (
+            <span>
+        FEDS201 {new Date().getFullYear()} ©{' '}
+                <a href="https://feds201.com" target="_blank">
+          CS-Club
+        </a>
+        .
+      </span>
+        )
+    },
+    editLink: {
+        text: 'Edit this page on GitHub',
+        href: 'https://github.com/Rochester-CS-Club/CS-CLUB-WEBSITE',
+    },
+    banner: {
+        key: '1.0-release',
+        text: (
+            <a href="#">
+                🎉 Programmers Handbook 1.0 is released. Read more →
+            </a>
+        )
+    },
+    feedback: {
+        content: 'Give Feedback',
+        labels: 'feedback,documentation',
+        useLink: () => 'https://github.com/Rochester-CS-Club/CS-CLUB-WEBSITE/issues/new/',
+    },
 }
