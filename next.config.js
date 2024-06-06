66e3bd64009fb485cee683fc7a7c3ba5fc2e90fa
RITESH RAJ AS <riteshrajas.arulselvan@gmail.com>
2024-06-05 20:27:58 -0400

    Initial COmmit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
+
 const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
-    themeConfig: './theme.config.jsx'
-})
+    themeConfig: './theme.config.jsx',
 
-module.exports = withNextra()
+})
+module.exports = withNextra({
+    footerText: `FEDS201 ${new Date().getFullYear()} © FEDS201.`
+    }
+)
+
+
 
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
