9097cf125280b5ae6595256670c7dc0633bdb973
RITESH RAJ AS <riteshrajas.arulselvan@gmail.com>
2024-06-10 19:58:38 -0400

    Added README.md file
    Signed-off-by: RITESH RAJ AS <riteshrajas.arulselvan@gmail.com>

--- theme.config.jsx
+++ theme.config.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 
 export default {
     logo: (
-       <>
-            <img src="https://i.imgur.com/9uqGKs6.png" alt="CS-Club" width="70" height="70" />
-            <span>&</span>
-            <img src="https://i.imgur.com/iiIB6ua.png" alt="FEDS201" width="70" height="70" />
-            <span>CS-Club</span>
-           
-       </>
+        <>
+            <img style={{borderRadius: '50%', marginTope: '10px'}} src="https://i.imgur.com/5qow8cH.png" alt="CS-Club" width="30" height="auto"/>
+            <span>&nbsp;</span>
+            {/*<span>&nbsp;</span>*/}
+            <img style={{borderRadius: '50%', marginTope: '10px'}} src="https://i.imgur.com/GmdZ72B.png" alt="FEDS201" width="30" height="30"/>
+            <span>&nbsp;</span>
+            <text >Programmers Handbook</text>
+        
+        </>
     ),
     
     project: {
@@ -21,12 +23,12 @@ export default {
         text: (
             <span>
                     <a href="https://feds201.com" target="_blank" rel="noopener noreferrer">
-                    FEDS201
+                    FEDS201 <span style={{color: '#888'}}></span>
                 </a>
                  {new Date().getFullYear()} ©{' '}
                 <a href="https://rhs-csclub.vercel.app" target="_blank" rel="noopener noreferrer">
                     CS-Club
-                </a>.
+                </a>
             </span>
         )
     },
